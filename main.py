b8a4a72ce7bdae54118c93770a69382cb4d01e93
Bineyam Getachew <bennewxoxo@gmail.com>
2024-03-13 14:26:37 +0300

    footer

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+    pdf.ln(278)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 100)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 pdf.output("output.pdf")
