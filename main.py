3d3ed5032b0cc7593df50f648e45dcd88071c4e2
Bineyam Getachew <bennewxoxo@gmail.com>
2024-05-01 09:17:13 +0300

    fix

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="a4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
-
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
@@ -14,14 +13,18 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
     for i in range(265):
-        pdf.cell(w=0, h=12, align="L", ln=1)
-        pdf.line(10, 21 + 10, 200, 21 + 10)
+        y = i * 10
+        pdf.line(10, 21 + y, 200, 21 + y)
+
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(20, 298, 10):
+            #range(start, stop, step)
+            pdf.line(10, y, 200, y)
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
